8073952cf7d6e598747a05b10b80af8566042414
nathan.latino <nathan.latino@he-arc.ch>
2020-04-21 17:29:01 +0200

    fix test

--- runTest.sh
+++ runTest.sh
@@ -2,4 +2,4 @@
 
 Xvfb :99 &
 
-/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/BoardelKatalon/SMBKatalon.prj" -retry=0 -testSuitePath="Test Suites/full_test" -executionProfile="default"  -browserType="Chrome (headless)" -Djava.awt.headless
+/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/boardelKatalon/boardelKatalon.prj" -retry=0 -testSuitePath="Test Suites/full_test" -executionProfile="default"  -browserType="Chrome (headless)" -Djava.awt.headless
--- src/test/java/ch/hearc/boardel/controller/RegisterControllerTest.java
+++ src/test/java/ch/hearc/boardel/controller/RegisterControllerTest.java
@@ -9,7 +9,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
